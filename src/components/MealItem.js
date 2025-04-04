ccdc3da36fc40803b58749edd12ea67ff56572dd
Margit Viigi <margit.viigi@voco.ee>
2025-04-04 11:21:53 +0300

    Yl 5

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,15 +1,20 @@
-const MealItem = (props, { meal }) => {
+const MealItem = ({ meal }) => {
+    const priceNew = new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: "EUR",
+    }).format(meal.price);
+
     return (
       <li className="meal-item">
         <article>
           <img
-            src={require(`../assets/${props.meal.image}`)} 
-            alt={props.meal.name}
+            src={require(`../assets/${meal.image}`)} 
+            alt={meal.name}
           />
           <div>
-            <h3>{props.meal.name}</h3>
-            <p className="meal-item-price">{props.meal.price}</p> 
-            <p className="meal-item-description">{props.meal.description}</p>
+            <h3>{meal.name}</h3>
+            <p className="meal-item-price">{priceNew}</p> 
+            <p className="meal-item-description">{meal.description}</p>
           </div>
           <p>
             <button className="meal-item-actions">Add to Cart</button>
